a9f71f3139117d1708d3e296a97e65a26b3d090c
DatBrian <vikard.2005@hotmail.com>
2023-06-06 02:45:05 -0500

    feat: :construction: Se añadió interactividad a los botones de la seccion de los detalles de la factura para añadir nuevos productos o sumar y restar la cantidad de un producto existente
    Se añadieron eventos de escucha a los botones y para agregar facturas, y sumar o restar cantidades de productos

--- js/my-buttons.js
+++ js/my-buttons.js
@@ -3,17 +3,45 @@ import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootst
 export class myButtons extends HTMLElement {
     constructor() {
         super();
+        this.container = null;
+        this.addButton = null;
+    }
+
+    handleEvent(e) {
+        if (e.target === this.addButton && e.type === "click") {
+            this.addProduct()
+        }
     }
 
     async components() {
         return await (await fetch("view/my-buttons.html")).text();
     }
 
-    connectedCallback() {
+    async connectedCallback() {
+
         document.adoptedStyleSheets.push(styles);
-        this.components().then(html => {
-            this.innerHTML = html;
-        })
+        await this.render();
+
+        this.addButton.addEventListener("click", this);
+
+    }
+
+    async render() {
+        let html = await this.components();
+        this.innerHTML = html;
+
+        this.container = this.querySelector("#containerProducts");
+        this.addButton = this.querySelector("#addButton");
+    }
+
+    addProduct() {
+        let newComponent = document.createElement("my-details");
+
+        let count = this.container.childElementCount;
+
+        newComponent.dataset.id = count;
+
+        this.container.insertAdjacentHTML("beforeend", newComponent.outerHTML);
     }
 }
 customElements.define("my-buttons", myButtons);
--- js/my-details.js
+++ js/my-details.js
@@ -3,17 +3,50 @@ import styles from "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootst
 export class myDetails extends HTMLElement {
     constructor() {
         super();
+        this.container = null;
+        this.plusButton = null;
+        this.lessButton = null;
+        this.input = null;
+    }
+
+    handleEvent(e) {
+        let container = e.target.parentNode.parentNode;
+        let input = container.querySelector("#amount");
+
+        e.target === this.plusButton && e.type === "click" ? this.plusAmount(input) :
+            e.target === this.lessButton && e.type === "click" ? this.lessAmount(input, container) : undefined;
     }
 
     async components() {
         return await (await fetch("view/my-details.html")).text();
     }
 
-    connectedCallback() {
+    async connectedCallback() {
         document.adoptedStyleSheets.push(styles);
-        this.components().then(html => {
-            this.innerHTML = html;
-        })
+        await this.render();
+
+        await this.addListeners(this.plusButton, this.lessButton);
+    }
+
+    async render() {
+        let html = await this.components();
+        this.innerHTML = html;
+
+        this.plusButton = this.querySelector(".plusAmount");
+        this.lessButton = this.querySelector(".lessAmount");
+    }
+
+    async addListeners(e1, e2) {
+        e1.addEventListener("click", this);
+        e2.addEventListener("click", this);
+    }
+
+    plusAmount(input) {
+        input.value++;
+    }
+
+    lessAmount(input, container) {
+        (input.value == 1) ? container.remove() : input.value--;
     }
 
 }
