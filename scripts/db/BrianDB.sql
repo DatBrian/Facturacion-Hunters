0a39468007ed8c93af03b1adedacf03a3a34726c
DatBrian <vikard.2005@hotmail.com>
2023-06-13 09:23:20 -0500

    feat: :card_file_box: Se agregó una tabla para facturas

--- scripts/db/BrianDB.sql
+++ scripts/db/BrianDB.sql
@@ -1,8 +1,14 @@
-CREATE DATABASE BrianDB;
-use BrianDB;
+CREATE DATABASE db_hunter_facture_brian;
+use db_hunter_facture_brian;
 CREATE TABLE tb_Users(
     cc INT(11) NOT NULL COMMENT 'Campo para el documento de identidad',
     name_com VARCHAR(57) NOT NULL COMMENT 'Campo para el nombre completo' ,
     age INTEGER(3) NOT NULL COMMENT 'Campo para la edad',
     PRIMARY KEY (cc)
-)
+);
+
+CREATE TABLE tb_Bill(
+    n_bill VARCHAR(25) NOT NULL COMMENT 'Número de factura',
+    bill_date DATETIME NOT NULL DEFAULT NOW() UNIQUE COMMENT 'Fecha de la factura',
+    PRIMARY KEY(n_bill)
+);
